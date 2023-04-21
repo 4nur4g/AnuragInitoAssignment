e033375af14332514e3fbeec4cf6b74a0fa42dbd
Anurag Pradhan <anuragp936@gmail.com>
2023-04-21 15:08:48 +0530

    Timer fixed

--- app/src/main/java/com/example/anuraginitoassignment/Screen2Activity.kt
+++ app/src/main/java/com/example/anuraginitoassignment/Screen2Activity.kt
@@ -3,6 +3,7 @@ package com.example.anuraginitoassignment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
 import com.example.anuraginitoassignment.databinding.ActivityScreen2Binding
 import com.github.lzyzsd.circleprogress.CircleProgress
@@ -21,12 +22,15 @@ class Screen2Activity : AppCompatActivity() {
             override fun onTick(millisUntilFinished: Long) {
                 val seconds = millisUntilFinished / 1000
                 binding.circularTimer.text = (seconds).toString() + 's'
-                binding.circularTimer.progress = ((millisUntilFinished/5000))*100.toFloat()
+//                binding.circularTimer.progress = ((millisUntilFinished/5000))*1000.toFloat()
+                binding.circularTimer.setDonut_progress(((seconds.toDouble()/5)*100).toInt().toString())
+                Log.d("TIMER", ((seconds.toDouble()/5)*100).toString())
             }
 
             override fun onFinish() {
                 binding.circularTimer.progress = 0f
-                binding.circularTimer.text = "0" + 's'
+                binding.circularTimer.text = "0s"
+                binding.circularTimer.setDonut_progress("0")
                 // move to Screen 3
             }
         }.start()
